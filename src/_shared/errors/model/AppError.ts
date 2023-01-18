6db0b70d0a80844780705def5b3ca779ee021e5c
guirra-byte <guirramatheus@gmail.com>
2023-01-18 17:24:13 -0300

    fix: atualizando repositório, base de dados e tratamento de erros

---
+++ src/_shared/errors/model/AppError.ts
@@ -0,0 +1,11 @@
+export class AppError{
+  message: string;
+  statusCode: number;
+  module: string;
+
+  constructor(message: string, statusCode = 400, module: string){
+    this.message = message;
+    this.statusCode = statusCode;
+    this.module = module;
+  }
+}
--- src/_shared/infra/http/routes/server.ts
+++ src/_shared/infra/http/routes/server.ts
@@ -1,9 +1,21 @@
-import { app } from "./routes/app";
-import { client } from "../client/prismaClient";
+import { app } from "../app";
+import { client } from "../../client/prismaClient";
 import cors from '@fastify/cors';
+import fastify from 'fastify';
+import { AppError } from "../../../errors/model/AppError";
 
 const _PORT = 3333;
 app.register(cors);
+
+fastify().setErrorHandler(function (_err, request, reply){
+  if(_err instanceof AppError){
+    this.log.error(_err);
+    return reply.status(_err.statusCode)
+    .send({ message: `${_err.message} --- ${_err.module}` })
+  } else {
+    return reply.send(_err)
+  }
+});
 
 app.listen({ port: _PORT, host: 'localhost' })
 .then(() => console.log('The Server already is running!'));
@@ -12,7 +24,7 @@ app.post('/habit', async () => {
   const habit = {
     title: 'Study the Bible',
   }
-
+  
   await client.habit.create({ data: { title: habit.title } });
 });
 
